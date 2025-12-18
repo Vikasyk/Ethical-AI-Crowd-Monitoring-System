a4c314991c889bbbbceb693282c161aa11212301
Vikasyk <vikasyk205@gmail.com>
2025-12-18 12:29:46 +0530

    Add files via upload

---
+++ download_video.py
@@ -0,0 +1,13 @@
+import yt_dlp
+
+url = "https://youtu.be/b8QZJ5ZodTs?si=tgN7ICXPM0NxACom"  # your link
+
+ydl_opts = {
+    'format': 'mp4',
+    'outtmpl': 'crowd_video3.mp4'
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("Download complete!")
